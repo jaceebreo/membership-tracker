3a2384eeaeca6b4e8db6645dd963aef8baf12609
jaceebreo <jcm.ebreo@gmail.com>
2025-12-18 23:11:22 +0800

    Set up memberships table

--- memberships/admin.py
+++ memberships/admin.py
@@ -1,9 +1,43 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
-from django.contrib.auth.models import User
-from .models import Role, StaffProfile, Member, Invoice, Billing
+from .models import StaffProfile, StaffRole, Member, MembershipType, Invoice, Billing, GymBranch
 
-admin.site.register(Role)
-admin.site.register(StaffProfile)
-admin.site.register(Member)
-admin.site.register(Invoice)
-admin.site.register(Billing)
+
+class BillingAdmin(ModelAdmin):
+    list_display = ("id", "member", "billing_date", "start_date",
+                    "end_date", "invoice__payment_amount")
+
+
+class MemberAdmin(ModelAdmin):
+    list_display = ("id", "email", "first_name", "last_name", "is_active")
+
+
+class MembershipTypeAdmin(ModelAdmin):
+    list_display = ("id", "name", "description")
+
+
+class RoleAdmin(ModelAdmin):
+    list_display = ("id", "role_name",)
+    ordering = ("id",)
+
+
+class InvoiceAdmin(ModelAdmin):
+    list_display = ("invoice_num", "member", "payment_date")
+
+
+class StaffProfileAdmin(ModelAdmin):
+    list_display = ("user",)
+
+
+class StaffRoleAdmin(ModelAdmin):
+    list_display = ("role_name",)
+
+
+# admin.site.register(Role, RoleAdmin)
+admin.site.register(StaffRole, StaffRoleAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(MembershipType, MembershipTypeAdmin)
+admin.site.register(Billing, BillingAdmin)
+admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(StaffProfile, StaffProfileAdmin)
+admin.site.register(GymBranch)
